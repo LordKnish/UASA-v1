29cca5839e1af7dda2be72ef210706d64f210ae7
Boaz <bernieice11@gmail.com>
2022-03-03 13:42:40 +0200

    Made fixes to calm the auto lint action down

--- UASA.py
+++ UASA.py
@@ -1,20 +1,20 @@
 #Created by Aurora Knish
 #Version 1.0.1
 
-import sounddevice as sd
 import numpy as np
 from datetime import datetime, timedelta
 import pytz
 import tweepy
 import telepot
 import time as tmtime
-global time5minutes
+global TIME5MINUTES
 from threading import Thread
 from time import sleep
 from pathlib import Path
 import yaml
 import os
-threadrunonce = False
+import sounddevice as sd
+THREADRUNONCE = False
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -28,34 +28,34 @@ def get_file_contents(filename): #Get the contents of the API key file
     return config
 
 def timer():
-    global threadrunonce
-    global time5minutes
-    global file
-    global channelid
-    global townname
+    global THREADRUNONCE
+    global TIME5MINUTES
+    global IMAGEFILE
+    global CHANNELID
+    global TOWNNAME
     sleep_duration = 4 #The time the sound should run until it considers it a siren
     Ukraine_Time = datetime.now(tz).strftime('%H:%M:%S')
     Ukraine_Date = datetime.now(tz).strftime('%d/%m/%Y')
     while sleep_duration > 0:
-        if(stop_thread == True):
-            print("Thread stopped at " + str(sleep_duration) + ' - ' + str(volume_norm))
+        if(STOP_THREAD == True):
+            print("Thread stopped at " + str(sleep_duration) + ' - ' + str(VOLUME_NORM))
             break
         else:
             print(f"Sound has {sleep_duration} seconds left until alert is triggered ")
             sleep(1)
             sleep_duration -= 1
     if(sleep_duration == 0 ):
-        print('(' + str(volume_norm) + ') Time: ' + str(Ukraine_Time) + '  -  SIREN')
-        if(datetime.strptime(Ukraine_Time,'%H:%M:%S') > datetime.strptime(time5minutes,'%H:%M:%S')):
-            text = 'Ukraine Siren Alert [' + str(Ukraine_Date) + ']\n\n' + Ukraine_Time + '\n' + townname + '\n\nMessage was generated using live stream data. If wrong please contact us.\n#Ukraine'
+        print('(' + str(VOLUME_NORM) + ') Time: ' + str(Ukraine_Time) + '  -  SIREN')
+        if(datetime.strptime(Ukraine_Time,'%H:%M:%S') > datetime.strptime(TIME5MINUTES,'%H:%M:%S')):
+            text = 'Ukraine Siren Alert [' + str(Ukraine_Date) + ']\n\n' + Ukraine_Time + '\n' + TOWNNAME + '\n\nMessage was generated using live stream data. If wrong please contact us.\n#Ukraine'
             
-            api.update_with_media(file,status=text)
+            api.update_with_media(IMAGEFILE,status=text)
             bot3 = telepot.Bot(apikeys['telepot']['api_key'])
-            bot3.sendPhoto(channelid, photo = open(file, 'rb'),caption=f'{text}',parse_mode="Markdown")
-            time5minutes = (datetime.strptime(Ukraine_Time,'%H:%M:%S') + timedelta(minutes=15)).strftime('%H:%M:%S')
-            print('Posting blocked until ' + time5minutes)
+            bot3.sendPhoto(CHANNELID, photo = open(IMAGEFILE, 'rb'),caption=f'{text}',parse_mode="Markdown")
+            TIME5MINUTES = (datetime.strptime(Ukraine_Time,'%H:%M:%S') + timedelta(minutes=15)).strftime('%H:%M:%S')
+            print('Posting blocked until ' + TIME5MINUTES)
     
-    threadrunonce = False
+    THREADRUNONCE = False
 
 
 def main():
@@ -65,22 +65,22 @@ def main():
     except KeyboardInterrupt:
         print("Stopped")
 def print_sound(indata, outdata, frames, time, status):
-    global volume_norm
-    global stop_thread
-    global threadrunonce
+    global VOLUME_NORM
+    global STOP_THREAD
+    global THREADRUNONCE
     try:
-        volume_norm = np.linalg.norm(indata)*10
-        print(volume_norm)
-        if(int(volume_norm) > 35): #volume level to trigger the script
-            stop_thread = False
+        VOLUME_NORM = np.linalg.norm(indata)*10
+        print(VOLUME_NORM)
+        if(int(VOLUME_NORM) > 35): #volume level to trigger the script
+            STOP_THREAD = False
             
-            if(threadrunonce == False):
-                threadrunonce = True
+            if(THREADRUNONCE is False):
+                THREADRUNONCE = True
                 timer_thread = Thread(target=timer)
                 
                 timer_thread.start()
         else:
-            stop_thread = True
+            STOP_THREAD = True
     except:
         quit
 
@@ -88,19 +88,19 @@ def print_sound(indata, outdata, frames, time, status):
 apikeys = get_file_contents('auth.yaml')
 auth = tweepy.OAuthHandler(apikeys['twitter']['consumer_key'], apikeys['twitter']['consumer_secret'])
 auth.set_access_token(apikeys['twitter']['access_token'], apikeys['twitter']['access_token_secret'])
-townname = 'Kyiv' #Set the towns name
+TOWNNAME = 'Kyiv' #Set the towns name
 import os
 print(os.name)
 if os.name == 'nt':
-    file = dname + '\\images\\' + townname + '.png' #Attempt to load the image file (Windows)
+    IMAGEFILE = dname + '\\images\\' + TOWNNAME + '.png' #Attempt to load the image file (Windows)
 else:
-    file = dname + '/images/' + townname + '.png' #Attempt to load the image file (Linux)
-channelid = -1001511264252 #Telegram channel ID
+    IMAGEFILE = dname + '/images/' + TOWNNAME + '.png' #Attempt to load the image file (Linux)
+CHANNELID = -1001511264252 #Telegram channel ID
 
 api = tweepy.API(auth) # Create API object
 tz = pytz.timezone('Europe/Kiev') # Set a timezone for the time of the alerts
-time5minutes = datetime.now(tz).strftime('%H:%M:%S')
-if Path(file).is_file():
+TIME5MINUTES = datetime.now(tz).strftime('%H:%M:%S')
+if Path(IMAGEFILE).is_file():
     print ("Image file exists!")
 else:
     print ("Image file does not exist. Closing program.")
